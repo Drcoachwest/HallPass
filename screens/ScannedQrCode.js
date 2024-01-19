e58f229137988407d1ef57e7c1171fc6fa6756b1
Michael West <michael.west@gpisd.org>
2024-01-19 09:17:45 -0600

    working on adding the cammera and qrcode scanner

---
+++ screens/ScannedQrCode.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import { Alert, StyleSheet } from "react-native";
+import QRCodeScanner from "react-native-qrcode-scanner";
+
+const ScannedQRCode = () => {};
+
+export default ScannedQRCode;
+
+9;
